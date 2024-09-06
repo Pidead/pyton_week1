230dc320637ecbbe1ec232ebc18206da8b8c0ba8
Pidead <136316969+Pidead@users.noreply.github.com>
2024-09-06 22:02:24 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#Title
+print("Welcome to the Python Quiz!")
+
+#Question
+
+print("Question 1:")
+print("What is the ouput of 2 + 2")
+print(f"a) 3 ")
+print(f"b) 4 ")
+print(f"c) 5 ")
+question_1 =input("")
+
+if question_1 == "b":
+    print(f"Your answer: {question_1}")
+    print("Correct!")
+elif question_1 == "a":
+    print(f"Your answer: {question_1}")
+    print(f"Incorrect!")
+elif question_1 == "c":
+    print(f"Your answer: {question_1}")
+    print("Incorrect!")    
+
+ 
